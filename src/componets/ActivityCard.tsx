20a74e43659940d78cdc46065c80be96d8fa435d
dhaifandito <dhaifan.da@gmail.com>
2022-02-23 21:19:08 +0700

    beranda page (without asset picture and responsive)

---
+++ src/componets/ActivityCard.tsx
@@ -0,0 +1,26 @@
+interface Props{
+    name: string
+    status: string
+    image: string
+    date:string
+    desc: string
+}
+
+export default function ActivityCard (props: Props){
+    return(
+          <div className="shadow bg-white rounded text-left m-3 scrChi d-flex flex-column justify-content-between" >
+            <div className="row p-3">
+                <img src={props.image} className="col-5"/>
+                <div className="col-7">     
+                    <p className="actDate noSpace">{props.date}</p>
+                    <p className="font-weight-bold noSpace" >{props.name}</p>
+                    <p className="noSpace">{props.desc}</p>
+                </div>
+            </div>
+            <div className="d-flex justify-content-between px-3 pt-2 noSpace bGray">
+                <p>{props.status}</p>
+                <p>...</p>
+            </div>
+        </div>
+    )
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "../pages/login";
+import AdminBeranda from "../pages/admin-beranda";
+import EmployeeBeranda from "../pages/employee-beranda";
+import ManagerBeranda from "../pages/manager-beranda";
 
 const Index = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/login" element={<Login />} />
+				<Route path="/admin/beranda" element={<AdminBeranda />} />
+				<Route path="/employee/beranda" element={<EmployeeBeranda />} />
+				<Route path="/manager/beranda" element={<ManagerBeranda />} />
 			</Routes>
 		</BrowserRouter>
 	);
