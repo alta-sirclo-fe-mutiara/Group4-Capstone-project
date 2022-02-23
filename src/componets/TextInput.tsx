116b55812178ee51638e14d34867869217a33f75
dhaifandito <dhaifan.da@gmail.com>
2022-02-23 16:18:30 +0700

    initial feature

---
+++ src/componets/TextInput.tsx
@@ -0,0 +1,43 @@
+import { ChangeEventHandler } from "react";
+
+type register = {
+	label: string;
+	type: string;
+	onChange: ChangeEventHandler;
+	placeholder?: any;
+	value?: string;
+};
+
+const TextInput = (register: register) => {
+	return (
+		<div className="form-group mt-3">
+			<p>
+				{register.label}
+				<span className="input text-danger">*</span>
+			</p>
+			<input
+				className="form-control"
+				type={register.type}
+				onChange={register.onChange}
+				required
+			/>
+		</div>
+	);
+};
+
+const TextInputAccount = (register: register) => {
+	return (
+		<div className="form-group mt-3">
+			<p className="mb-1 form-label">{register.label}</p>
+			<input
+				className="form-control"
+				type={register.type}
+				onChange={register.onChange}
+				placeholder={register.placeholder}
+				value={register.value}
+			/>
+		</div>
+	);
+};
+
+export { TextInput, TextInputAccount };
